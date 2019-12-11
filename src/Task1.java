8b5270ab6289710ca442c001c2f5e14c418f83ab
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-11 20:57:50 +0400

    add sortInDescendingOrder

--- src/Task1.java
+++ src/Task1.java
@@ -60,6 +60,17 @@ public class Task1 {
             }
         }
     }
+    public static void sortInDescendingOrder(double [] arr){
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = 0; j <arr.length-i-1 ; j++) {
+                if(arr[j]<arr[j+1]) {
+                    double temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
 
     }
