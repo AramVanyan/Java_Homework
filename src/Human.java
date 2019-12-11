7945ab506eb5cba9d5fb6ea27cdba3b4dd1efe7f
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-11 21:49:29 +0400

    add Human class

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+    public Human(String firstName,String lastName,String gender,int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+    }
+
+}
